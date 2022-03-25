57845f6e1ae43acbf9d91e407cc754adbec6193b
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2022-03-25 21:40:03 +0000

    Add failsafe in case of missing 0, 0 by more than destination threshold

--- src/move_figure_of_eight.py
+++ src/move_figure_of_eight.py
@@ -8,20 +8,23 @@ from math import pi, sqrt, atan
 
 INITIAL_DISTANCE_THRESHOLD = 0.3
 DESTINATION_THRESHOLD = 0.05
-LINEAR_VELOCITY = 0.13
 CIRCLE_RADIUS = 0.5
+TIME_PER_LOOP = 28
+LINEAR_VELOCITY = 2 * pi * CIRCLE_RADIUS / TIME_PER_LOOP
 ANGULAR_VELOCITY = LINEAR_VELOCITY / CIRCLE_RADIUS
 ANGLE_PRECISION = 0.01
-ANGLE_CORRECTION_SPEED = 0.2
-DISTANCE_CORRECTION_PRECISION = 0.005
-DISTANCE_CORRECTION_SPEED = 0.05
+ANGLE_CORRECTION_SPEED = 0.25
+DISTANCE_CORRECTION_PRECISION = 0.02
+DISTANCE_CORRECTION_SPEED = 0.08
+FREQUENCY = 50
+
 
 class Main():
     def __init__(self):
         self.node_name = "move_figure_of_eight"
 
         rospy.init_node(self.node_name)
-        self.rate = rospy.Rate(10)  # hz
+        self.rate = rospy.Rate(FREQUENCY)  # hz
 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
@@ -32,27 +35,27 @@ class Main():
         self.odom_data = Odom_data()
 
         self.is_loop_1 = True
-        self.initial_movement = False
         self.message_iteration = 1
-    
+        self.distance_travelled = 0
+        self.position_previous_iteration_x = 0
+        self.position_previous_iteration_y = 0
+
     def loop_1_completed(self):
-        if not self.initial_movement:
-            if sqrt(self.odom_data.posx**2+self.odom_data.posy**2) >= INITIAL_DISTANCE_THRESHOLD:
-                self.initial_movement = True
-        elif sqrt(self.odom_data.posx**2+self.odom_data.posy**2) <= DESTINATION_THRESHOLD:
-            self.initial_movement = False
+        if (sqrt(self.odom_data.posx**2+self.odom_data.posy**2) <= DESTINATION_THRESHOLD and self.distance_travelled > INITIAL_DISTANCE_THRESHOLD
+            ) or self.distance_travelled >= 2*pi*CIRCLE_RADIUS:
+
             self.is_loop_1 = False
             self.correct_position()
+            self.distance_travelled = 0
 
     def loop_2_completed(self):
-        if not self.initial_movement:
-            if sqrt(self.odom_data.posx**2+self.odom_data.posy**2) >= INITIAL_DISTANCE_THRESHOLD:
-                self.initial_movement = True
-        elif sqrt(self.odom_data.posx**2+self.odom_data.posy**2) <= DESTINATION_THRESHOLD:
+        if (sqrt(self.odom_data.posx**2+self.odom_data.posy**2) <= DESTINATION_THRESHOLD and self.distance_travelled > INITIAL_DISTANCE_THRESHOLD
+            ) or self.distance_travelled >= 2*pi*CIRCLE_RADIUS:
+
             self.correct_position()
+            self.distance_travelled = 0
             print("Manoeuver completed successfully")
             self.shutdownhook()
-
 
     def shutdownhook(self):
         print(f"Stopping the '{self.node_name}' node at: {rospy.get_time()}")
@@ -63,34 +66,60 @@ class Main():
         def sign(x):
             return 1 if x >= 0 else -1
 
-        angle = -atan(self.odom_data.posy/self.odom_data.posx)
+        angle = atan(self.odom_data.posy/self.odom_data.posx)
+        reverse = False
+
+        if self.odom_data.posx >= 0:
+            reverse = True
 
         if sqrt(self.odom_data.posx**2+self.odom_data.posy**2) > DISTANCE_CORRECTION_PRECISION:
             while abs(angle - self.odom_data.angle) - ANGLE_PRECISION > 0 and not self.ctrl_c:
-                self.publish_velocity.publish_velocity(0, sign(angle - self.odom_data.angle)*ANGLE_CORRECTION_SPEED)
-            
+                self.publish_velocity.publish_velocity(
+                    0, sign(angle - self.odom_data.angle)*ANGLE_CORRECTION_SPEED)
+                self.print_data()
+                self.rate.sleep()
+
             while sqrt(self.odom_data.posx**2+self.odom_data.posy**2) > DISTANCE_CORRECTION_PRECISION and not self.ctrl_c:
-                self.publish_velocity.publish_velocity(DISTANCE_CORRECTION_SPEED, 0)
+                self.publish_velocity.publish_velocity(
+                    DISTANCE_CORRECTION_SPEED if not reverse else -DISTANCE_CORRECTION_SPEED, 0)
+                self.print_data()
+                self.rate.sleep()
 
         while abs(self.odom_data.angle) - ANGLE_PRECISION > 0 and not self.ctrl_c:
-            self.publish_velocity.publish_velocity(0, sign(-self.odom_data.angle)*ANGLE_CORRECTION_SPEED)
+            self.publish_velocity.publish_velocity(
+                0, sign(-self.odom_data.angle)*ANGLE_CORRECTION_SPEED)
+            self.print_data()
+            self.rate.sleep()
+
+    def print_data(self):
+        self.message_iteration += 1
+        if self.message_iteration > FREQUENCY:
+            self.message_iteration = 1
+            print(self.odom_data.output_string)
+
+    def update_distance_travelled(self):
+        self.distance_travelled += sqrt((self.odom_data.posx - self.position_previous_iteration_x)**2+(
+            self.odom_data.posy - self.position_previous_iteration_y)**2)
+
+        self.position_previous_iteration_x = self.odom_data.posx
+        self.position_previous_iteration_y = self.odom_data.posy
 
     def main_loop(self):
         while not self.ctrl_c:
-            self.message_iteration += 1
-            if self.message_iteration > 10:
-                self.message_iteration = 1
-                print(self.odom_data.output_string)
+            self.print_data()
+            self.update_distance_travelled()
 
             if self.is_loop_1:
                 self.loop_1_completed()
             else:
                 self.loop_2_completed()
 
-            angular_vel = ANGULAR_VELOCITY if self.is_loop_1 else -ANGULAR_VELOCITY # rad/s
-            self.publish_velocity.publish_velocity(LINEAR_VELOCITY, angular_vel)
+            angular_vel = ANGULAR_VELOCITY if self.is_loop_1 else -ANGULAR_VELOCITY  # rad/s
+            self.publish_velocity.publish_velocity(
+                LINEAR_VELOCITY, angular_vel)
 
             self.rate.sleep()
+
 
 class Publish_velocity():
 
@@ -98,7 +127,7 @@ class Publish_velocity():
         topic_name = "cmd_vel"
         self.pub = rospy.Publisher(topic_name, Twist, queue_size=10)
 
-    def publish_velocity(self, linear = 0, angular = 0):
+    def publish_velocity(self, linear=0, angular=0):
         vel_cmd = Twist()
         vel_cmd.linear.x = linear
         vel_cmd.angular.z = angular
