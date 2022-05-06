291a8c70a012c5e382e83574f835399f0d5bc9b1
Robotics Student <student@TENB885849EB7F0.shefuniad.shef.ac.uk>
2022-05-06 13:46:00 +0100

    .

--- src/maze.py
+++ src/maze.py
@@ -5,7 +5,8 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from math import pi, sqrt
-
+from sensor_msgs.msg import LaserScan
+import numpy as np
 FREQUENCY = 100
 
 class Main():
@@ -131,4 +132,41 @@ if __name__ == '__main__':
         main_instance = Main()
         main_instance.main_loop()
     except rospy.ROSInterruptException:
+        pass
+
+
+class Forward:
+
+    def _init_(self):
+        node_name = "maze_navigation"
+
+        
+      
+
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(10) # hz
+
+        self.vel = Twist()
+
+        self.ctrl_c = False
+        rospy.on_shutdown(self.shutdownhook)
+
+        self.wall_distance = []
+
+        self.right_side_wall_distance = []
+        self.left_side_wall_distance = []
+
+    def callback_lidar(self, lidar_data):
+        left_arc = lidar_data.ranges[0:10]
+        right_arc = lidar_data.ranges[-10:]
+        front_arc = np.array(left_arc + right_arc)
+
+
+
+
+    def shutdownhook(self):
+        self.shutdown_function()
+        self.ctrl_c = True    
+
+        
         pass
