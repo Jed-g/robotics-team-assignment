e179285894c6fd308f606f8ec6428bb79dafc5b5
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2022-05-19 20:02:36 +0100

    Create partial solution for task 2 & 3

--- src/task2.py
+++ src/task2.py
@@ -7,13 +7,14 @@ from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
 from math import inf, pi, sqrt, sin, cos, atan2
 import numpy as np
+import time
 
 FREQUENCY = 10
 LINEAR_VELOCITY = 0.25
-ANGULAR_VELOCITY = 0.4
+ANGULAR_VELOCITY = 0.8
 RANGE_THRESHOLD = 1.5
-CLEARANCE_THRESHOLD = 0.3
-FORWARD_STOPPING_THRESHOLD = 0.4
+CLEARANCE_THRESHOLD = 0.35
+FORWARD_STOPPING_THRESHOLD = 0.45
 WALL_PROXIMITY_THRESHOLD = 0.4
 WALL_PROXIMITY_THRESHOLD_PRECISION = 0.05
 MAXIMUM_WALL_DISTANCE_CORRECTION_ANGLE = 30
@@ -23,6 +24,7 @@ CONVEX_CORNER_THRESHOLD = 0.3
 ANGLE_CORRECTION_SPEED = 1
 CONVEX_CORNER_CORRECTION_SPEED = 1
 STOP_TO_CORRECT_ANGLE_THRESHOLD = 10
+REVERSE_TIME = 1
 
 class Main():
     def __init__(self):
@@ -77,7 +79,7 @@ class Main():
                         return
                     
                     if lin_speed == None:
-                        self.publish_velocity.publish_velocity(LINEAR_VELOCITY, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
+                        self.publish_velocity.publish_velocity(0, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
                     else:
                         self.publish_velocity.publish_velocity(lin_speed, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
 
@@ -88,7 +90,7 @@ class Main():
                         return
                 
                 if lin_speed == None:
-                    self.publish_velocity.publish_velocity(LINEAR_VELOCITY, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
+                    self.publish_velocity.publish_velocity(0, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
                 else:
                     self.publish_velocity.publish_velocity(lin_speed, -ANGULAR_VELOCITY if ang_speed == None else -ang_speed)
 
@@ -103,7 +105,7 @@ class Main():
                         return
                     
                     if lin_speed == None:
-                        self.publish_velocity.publish_velocity(LINEAR_VELOCITY, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
+                        self.publish_velocity.publish_velocity(0, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
                     else:
                         self.publish_velocity.publish_velocity(lin_speed, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
 
@@ -113,7 +115,7 @@ class Main():
                         return
 
                 if lin_speed == None:
-                    self.publish_velocity.publish_velocity(LINEAR_VELOCITY, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
+                    self.publish_velocity.publish_velocity(0, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
                 else:
                     self.publish_velocity.publish_velocity(lin_speed, ANGULAR_VELOCITY if ang_speed == None else ang_speed)
 
@@ -131,14 +133,14 @@ class Main():
         return array
 
     def can_move_forward(self):
-        data = list(self.lidar_data.ranges[-30:])
-        data.extend(list(self.lidar_data.ranges[:30]))
+        data = list(self.lidar_data.ranges[-35:])
+        data.extend(list(self.lidar_data.ranges[:35]))
 
         if min(data) < CLEARANCE_THRESHOLD:
             return False
 
-        data = list(self.lidar_data.ranges[-5:])
-        data.extend(list(self.lidar_data.ranges[5:]))
+        data = list(self.lidar_data.ranges[-2:])
+        data.extend(list(self.lidar_data.ranges[:2]))
 
         if min(data) < FORWARD_STOPPING_THRESHOLD:
             return False
@@ -231,17 +233,20 @@ class Main():
     def main_loop(self):
         while not self.ctrl_c:
             if self.odom_data.initial_data_loaded and self.lidar_data.initial_data_loaded:
-                
-                if self.can_move_forward():
-                    if not self.is_convex_corner():
-                        self.follow_left_wall()
-                        self.publish_velocity.publish_velocity(LINEAR_VELOCITY, 0)
-                    else:
-                        self.do_turn()
+
+                if not len(self.lidar_data.get_space_array()) == 0:
+                    self.turn_to_angle_360_system(self.offset_space_array(self.lidar_data.get_space_array())[0][0])
                 else:
-                    self.publish_velocity.publish_velocity()
-                    angle_to_turn = self.odom_data.angle_360 - 1
-                    self.turn_to_angle_360_system(angle_to_turn if angle_to_turn >= 0 else 358)
+                    angle_to_turn = self.odom_data.angle_360 + 180
+                    self.turn_to_angle_360_system(angle_to_turn if angle_to_turn < 360 else angle_to_turn - 360)
+
+                while self.can_move_forward():
+                    self.publish_velocity.publish_velocity(LINEAR_VELOCITY, 0)
+                    if self.ctrl_c:
+                        break
+                self.publish_velocity.publish_velocity(-LINEAR_VELOCITY, 0)
+                time.sleep(REVERSE_TIME)
+                self.publish_velocity.publish_velocity()
 
                 self.rate.sleep()
 
@@ -369,7 +374,7 @@ class Lidar_data():
                     previous_inf = False
         
         if not first_angle_set:
-            return None
+            return [(0, 360)]
 
         angle_after_last_obstacle = 0 if beginning_of_current_space == None else 360 - beginning_of_current_space
 
